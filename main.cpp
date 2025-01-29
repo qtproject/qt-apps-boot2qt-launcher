9fb7f723cf2bcf840075e361d473f82015d42334
Samuli Piippo <samuli.piippo@qt.io>
2025-01-29 11:14:25 +0200

    Support threaded rendering for Nvidia EGLStreams
    Compositor needs use Qt::AA_ShareOpenGLContexts application attribute
    on NVidia boards.
    
    Fixes: QTBUG-131870
    Change-Id: Ic658da5b8ab1ac097a66085f5f53446a7039f7bd
    Reviewed-by: Pasi Petäjäjärvi <pasi.petajajarvi@qt.io>

--- main.cpp
+++ main.cpp
@@ -29,6 +29,9 @@ void displayHelp(const char *appName)
 
 int main(int argc, char **argv)
 {
+    // ShareOpenGLContexts is needed for using the threaded renderer
+    // on Nvidia EGLStreams
+    QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts, true);
     QGuiApplication app(argc, argv);
 
     QString appsRoot;
